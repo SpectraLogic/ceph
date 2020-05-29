5fbdd3c6184b2723cc6f17a7ca9746b2247165bb
baukus <baukus@github.com>
2020-05-29 13:55:17 -0600

    Override local ubuntu release name. The packaging script make-debs.sh will generate a ubuntu repo defaulting to $(lsb_release -sc) which on our current build machines is "bionic" but this branch is explicitly targeting "focal"
    modified: JenkinsfileUPS
    - Define a Jenkins environment variable UBUNTU_CODENAME="focal"
      that can override this assignment "codename=$(lsb_release -sc)" in
      make-deps.sh
    
    modified:  make-debs.sh
    - Add code to use UBUNTU_CODENAME from the environment if set.

--- make-debs.sh
+++ make-debs.sh
@@ -17,7 +17,12 @@
 set -xe
 
 base=${1:-/tmp/release}
-codename=$(lsb_release -sc)
+if [ -n "$UBUNTU_CODENAME" ] 
+then
+	codename="$UBUNTU_CODENAME"
+else
+	codename=$(lsb_release -sc)
+fi
 releasedir=$base/$(lsb_release -si)/WORKDIR
 rm -fr $(dirname $releasedir)
 mkdir -p $releasedir
