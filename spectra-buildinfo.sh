6083ee450833fa198872d193b3aa900ec39306d3
baukus <baukus@github.com>
2020-06-03 11:06:22 -0600

    Don't try and used datel BUILD_NUMBER is sufficient.

--- spectra-buildinfo.sh
+++ spectra-buildinfo.sh
@@ -5,7 +5,8 @@
 PROG=$(basename $0)
 OUTPUT="${1:-none}" # Output file is assumed to be in the artifacts directory
 BRANCH="${2:-none}" # Branch
-BLDNUM="${3:-none}" # Jenkins build number
+VERSION="${3:-none}" # Version from tag
+BLDNUM="${4:-none}"  # Jenkins build number
 shift 3
 ARTIFACTS="$@"		# Files and directories that are our "artifacts" relative to dirname $OUTPUT
 PFMT="%-15s %s\n"
@@ -17,9 +18,10 @@ error()
 	exit 3
 }
 
-[ "$OUTPUT" == "none" ] && error "no output file specified"
-[ "$BRANCH" == "none" ] && error "no branch specified"
-[ "$BLDNUM" == "none" ] && error "no build number specified"
+[ "$OUTPUT" == "none" ]  && error "no output file specified"
+[ "$BRANCH" == "none" ]  && error "no branch specified"
+[ "$VERSION" == "none" ] && error "no version specified"
+[ "$BLDNUM" == "none" ]  && error "no build number specified"
 
 GIT_COMMIT="$(git rev-parse HEAD)"
 
@@ -28,8 +30,9 @@ file=$(basename $OUTPUT)
 cd $dir || error "cd $dir failed"
 
 printf "$PFMT" "DATE:" "$(date +%Y%m%d.%H:%M)" > $file || error "failed to create $OUTPUT"
-printf "$PFMT" "BUILD_NUMBER:" "$BLDNUM" >> $file
 printf "$PFMT" "BRANCH:" "$BRANCH" >> $file
+printf "$PFMT" "VERSION:" "$VERSION" >> $file
+printf "$PFMT" "BUILD_NUMBER:" "$BLDNUM" >> $file
 printf "$PFMT" "GIT_COMMIT:" "$GIT_COMMIT" >> $file
 printf "$PFMT" "FILES:" >> $file
 find $ARTIFACTS >> $file
